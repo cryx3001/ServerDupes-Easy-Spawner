b102b652f7406eed325e32b6ce43a86f182174f9
Titouan Guionneau <titouan.guionneau@epita.fr>
2025-06-27 18:19:34 +0200

    Fixed typo

--- lua/easy_spawner/sh_srvdupe_es.lua
+++ lua/easy_spawner/sh_srvdupe_es.lua
@@ -1,5 +1,5 @@
 function SrvDupeES.CheckPlyWritePermissions(ply)
     local roles = SrvDupeES.Config.AllowedRolesWrite or {}
     local steamIDs = SrvDupeES.Config.AllowedSteamIDWrite or {}
-    return table.HasValue(roles, ply:GetUserGroup()) or table.HasValue(steamIDs, ply:GetSteamID())
+    return table.HasValue(roles, ply:GetUserGroup()) or table.HasValue(steamIDs, ply:SteamID())
 end
